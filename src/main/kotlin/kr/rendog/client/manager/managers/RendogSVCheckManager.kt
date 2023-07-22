7f9cf16350169d8de74eaed607e8bdcaa6cfa615
MellDa <meltdown1024@naver.com>
2023-07-22 16:36:27 +0900

    fix: RendogSVCheckManager
    now checks singleplayer.
    make a delay for 1 second before log out.

--- src/main/kotlin/kr/rendog/client/manager/managers/RendogSVCheckManager.kt
+++ src/main/kotlin/kr/rendog/client/manager/managers/RendogSVCheckManager.kt
@@ -1,25 +1,34 @@
 package kr.rendog.client.manager.managers
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kr.rendog.client.RendogMod
 import kr.rendog.client.event.SafeClientEvent
 import kr.rendog.client.manager.Manager
+import kr.rendog.client.util.threads.defaultScope
 import kr.rendog.client.util.threads.safeListener
 import net.minecraft.network.play.server.SPacketDisconnect
 import net.minecraft.util.text.TextComponentString
 import net.minecraftforge.fml.common.gameevent.TickEvent
 
 object RendogSVCheckManager : Manager {
-    private val rendogSvIP = arrayOf("rendog.kr", "global.rendog.kr", "private-scn-sev99.scn.pw:1799")
-
+    private val rendogSVIP = arrayOf("rendog.kr", "global.rendog.kr", "private-scn-sev99.scn.pw:1799")
     init {
         safeListener<TickEvent.ClientTickEvent> {
+            if (it.phase != TickEvent.Phase.END) return@safeListener
+            if (mc.isSingleplayer) logDelay("RendogClient is only available in RendogServer.")
             val ip = mc.currentServerData?.serverIP ?: return@safeListener
-            if (ip.lowercase() !in rendogSvIP) {
-                log("RendogClient is only available in RendogServer.")
+            if (ip.lowercase() !in rendogSVIP) {
+                logDelay("RendogClient is only available in RendogServer.")
             }
         }
 
     }
-    private fun SafeClientEvent.log(msg : String) {
-        connection.handleDisconnect(SPacketDisconnect(TextComponentString(msg)))
+    private fun SafeClientEvent.logDelay(msg : String) {
+        defaultScope.launch {
+            RendogMod.LOG.info("Player is not in RendogServer! Will disconnect in 1 second, maybe crash in SinglePlayer...")
+            delay(1000L)
+            connection.handleDisconnect(SPacketDisconnect(TextComponentString(msg)))
+        }
     }
 }
